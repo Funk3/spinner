dc0bf21b503293137864b0a21e0efb6350e1983d
Zak <zak.johansson@gmail.com>
2022-11-22 13:15:29 -0800

    played around with \r

---
+++ spinner1.js
@@ -0,0 +1,18 @@
+// process.stdout.write('hello from spinner1.js... \rheyyy\n');
+
+setTimeout(() => {
+  process.stdout.write('\r| my name is mud   ');
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\r/ who are you  ');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r- I dunno what Im doing  ');
+}, 500);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\  oh god '); 
+}, 700);

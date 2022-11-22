2e363fca226d28d0ae7446fe22f30275919418ee
Zak <zak.johansson@gmail.com>
2022-11-22 13:22:22 -0800

    refactored spinner2

--- spinner2.js
+++ spinner2.js
@@ -1,16 +1,25 @@
-setTimeout(() => {
-  process.stdout.write('\r| my name is mud');
-}, 100);
+let spinner = "|/-\\";
 
-setTimeout(() => {
-  process.stdout.write('\r/ who are you');
-}, 300);
+for (let i = 0; i < spinner.length; i++) {
+  setTimeout(() => {
+    process.stdout.write("\r" + spinner[i]);
+  }, 200 * i)
+}
 
-setTimeout(() => {
-  process.stdout.write('\r- I dunno what Im doing');
-}, 500);
 
-setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
-  process.stdout.write('\r\\  oh god'); 
-}, 700);
+// setTimeout(() => {
+//   process.stdout.write('\r|   ');
+// }, 100);
+
+// setTimeout(() => {
+//   process.stdout.write('\r/   ');
+// }, 300);
+
+// setTimeout(() => {
+//   process.stdout.write('\r-   ');
+// }, 500);
+
+// setTimeout(() => {
+//   // Need to escape the backslash since it's a special character.
+//   process.stdout.write('\r\\   '); 
+// }, 700);
